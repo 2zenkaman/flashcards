8a1d705dcf7e7973d0ad75398531219cdf3ae2d1
Evgeniy <zhsliborskii@gmail.com>
2026-04-24 19:45:03 +0300

    Remove: exported function

--- static/form.js
+++ static/form.js
@@ -37,14 +37,3 @@ form.onsubmit = async (ev) => {
         return
     }
 }
-
-window.onload = async () => {
-    try {
-        const resp = await fetch('/api/cards')
-        const cards = await resp.json()
-
-        cards.forEach(c => deck.appendChild(Row(c)))
-    } catch (e) {
-        console.error(e)
-    }
-} 
