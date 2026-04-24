bd476a0269670268f39bf452f481cd2f076f3c96
Evgeniy <zhsliborskii@gmail.com>
2026-04-24 18:48:55 +0300

    Fix: no closing tags in markup

--- static/row.js
+++ static/row.js
@@ -2,10 +2,10 @@ export default function Row({id, question, answer, learned}) {
     const row = document.createElement('div')
     row.className = "flashcards-row"
     row.innerHTML = `
-        <div class="flashcards-cell cell-1">${id}<div>
-        <div class="flashcards-cell cell-2">${question}<div>
-        <div class="flashcards-cell cell-3">${answer}<div>
-        <div class="flashcards-cell cell-4">${learned}<div>
+        <div class="flashcards-cell cell-1">${id}</div>
+        <div class="flashcards-cell cell-2">${question}</div>
+        <div class="flashcards-cell cell-3">${answer}</div>
+        <div class="flashcards-cell cell-4">${learned}</div>
     `
 
     return row;
