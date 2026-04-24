f1eb4a4c21307faeed508e8c4cd138a87be76119
Evgeniy <zhsliborskii@gmail.com>
2026-04-25 01:01:58 +0300

    Move: EVERYTHING In the same place

--- static/main.js
+++ static/main.js
@@ -1,9 +1,26 @@
 "use strict";
 
-import Row from "./row.js";
+import Row from "./row.js"
 
 const form = document.querySelector('form')
 const deck = document.querySelector('#flashcards-deck > tbody')
+
+window.onload = async () => {
+    document.querySelector('form').querySelectorAll('input').forEach(i => i.value = '')
+
+    try {
+        const resp = await fetch('/api/cards')
+        const data = await resp.json()
+
+        if (!resp.ok) {
+            throw new Error(data.error)
+        }
+
+        data.forEach(c => deck.appendChild(Row(c)))
+    } catch (e) {
+        console.error(e)
+    }
+}
 
 // document.querySelector('form').addEventListener('submit', (ev) => {...})
 form.onsubmit = async (ev) => {
