e4d2f6e33ee6862c0b4543cb1dac623b9226bc37
Evgeniy <zhsliborskii@gmail.com>
2026-04-24 15:49:06 +0300

    Implement: api handlers

---
+++ handlers/api.go
@@ -0,0 +1,27 @@
+package handlers
+
+import "net/http"
+
+func HandleCreateCard() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func HandleGetAllCards() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func HandleUpdateCard() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func HandleDeleteCard() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
--- server.go
+++ server.go
@@ -1,9 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"flashcards/handlers"
+	"net/http"
+)
 
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /api/cards", handlers.HandleCreateCard())
+	mux.HandleFunc("GET /api/cards", handlers.HandleGetAllCards())
+	mux.HandleFunc("PUT /api/cards", handlers.HandleUpdateCard())
+	mux.HandleFunc("DELETE /api/cards", handlers.HandleDeleteCard())
 
 	http.ListenAndServe(":8080", mux)
 }
