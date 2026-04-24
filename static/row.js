d017293f9e741c98cf13d911954e6eeec63168b2
Evgeniy <zhsliborskii@gmail.com>
2026-04-24 19:48:25 +0300

    Add: card removal logic

--- static/row.js
+++ static/row.js
@@ -6,7 +6,29 @@ export default function Row({id, question, answer, learned}) {
         <td class="flashcards-cell cell-question">${question}</td>
         <td class="flashcards-cell cell-answer">${answer}</td>
         <td class="flashcards-cell cell-learned">${learned}</td>
+        <td class="flashcards-cell cell-delete"></td>
     `
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = async () => {
+        try {
+            const resp = await fetch(`/api/cards/${id}`, {
+                method: 'DELETE',
+            })
+
+            if (!resp.ok) {
+                throw new Error("Unable to delete")
+            }
+
+        } catch (e) {
+            return console.error(e)
+        }
+
+        row.remove()
+    }
+
+    row.querySelector('td.cell-delete').append(deleteButton)
 
     return row;
 }
