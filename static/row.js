d87fc85670af04774fd96738ca717974b18a5b39
Evgeniy <zhsliborskii@gmail.com>
2026-04-24 19:05:55 +0300

    Refactor: switch to table' vyorstkagit add static/row.js static/index.html

--- static/row.js
+++ static/row.js
@@ -1,11 +1,11 @@
 export default function Row({id, question, answer, learned}) {
-    const row = document.createElement('div')
+    const row = document.createElement('tr')
     row.className = "flashcards-row"
     row.innerHTML = `
-        <div class="flashcards-cell cell-1">${id}</div>
-        <div class="flashcards-cell cell-2">${question}</div>
-        <div class="flashcards-cell cell-3">${answer}</div>
-        <div class="flashcards-cell cell-4">${learned}</div>
+        <td class="flashcards-cell cell-id">${id}</td>
+        <td class="flashcards-cell cell-question">${question}</td>
+        <td class="flashcards-cell cell-answer">${answer}</td>
+        <td class="flashcards-cell cell-learned">${learned}</td>
     `
 
     return row;
