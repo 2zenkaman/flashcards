9d78ae83da886d5929c99fd18f10a562fce80ac6
Evgeniy <zhsliborskii@gmail.com>
2026-04-25 01:26:52 +0300

    Add: window fabricator function

---
+++ static/learn.js
@@ -0,0 +1,17 @@
+export default function Window({question, answer, learned}) {
+    const window = document.createElement('div')
+    window.className = 'window'
+    window.dataset.learned = learned
+    window.innerHTML = `
+        <div>${question}</div>
+        <div hidden>${answer}</div>
+    `
+
+    window.onclick = () => {
+        window.childNodes.forEach(c => {
+            c.hidden = !c.hidden
+        })
+    }
+
+    return window
+}
